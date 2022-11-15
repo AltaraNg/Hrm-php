e85609a21d4ff3c66200114b4870f0d2cf7e9895
harmlessprince <realolamilekan@gmail>
2022-11-15 11:20:36 +0100

    added response helpers

---
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+    public  function login(LoginRequest $request){
+
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,54 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * User model class
+ * @package App\Models
+ * @property-read  int $id
+ * @property int $role_id
+ * @property int $staff_id
+ * @property string $full_name
+ * @property Carbon $date_of_appointment
+ * @property Carbon $date_of_exit
+ * @property string $status
+ * @property string $category
+ * @property string $phone_number
+ * @property string $highest_qualification
+ * @property int $branch_id
+ * @property string $password;
+ * @property string $gender
+ * @property string $referee_1
+ * @property string $referee_2
+ * @property string $referee_1_phone_no
+ * @property string $referee_2_phone_no
+ * @property Carbon $date_of_birth
+ * @property int $hr_id
+ * @property string $nationality
+ * @property string $next_of_kin_name
+ * @property string $next_of_kin_phone_no
+ * @property string $guarantor_name
+ * @property int $portal_access
+ * @property string $api_token
+ * @property string $remember_token
+ * @property string $guarantor_phone_no
+ * @property string $guarantor_address
+ * @property string $guarantor_relationship
+ * @property string $guarantor_name_2
+ * @property string $guarantor_phone_no_2
+ * @property string $guarantor_address_2
+ * @property string $guarantor_relationship_2
+ * @property string $cv_url
+ *
+ * @property-read  Carbon $created_at
+ * @property-read  Carbon $updated_at
+ * Relationships
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -17,11 +60,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
