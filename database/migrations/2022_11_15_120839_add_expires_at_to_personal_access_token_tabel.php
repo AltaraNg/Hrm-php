47b48df46bd7048764116492860298d8f0caca0f
harmlessprince <realolamilekan@gmail>
2022-11-17 13:09:42 +0100

    added spatie role and permission table

--- database/migrations/2022_11_15_120839_add_expires_at_to_personal_access_token_tabel.php
+++ database/migrations/2022_11_15_120839_add_expires_at_to_personal_access_token_tabel.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('personal_access_tokens', function (Blueprint $table) {
-            //
+            $table->dropColumn('expires_at');
         });
     }
 };
