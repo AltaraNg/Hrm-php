66744683c1e1e271b39ef359376d9e741a1a3cb3
harmlessprince <realolamilekan@gmail>
2022-11-18 09:33:12 +0100

    role and permissions can now be created

---
+++ app/Http/Controllers/RolePermissionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RolePermissionController extends Controller
+{
+    public function assignPermissionToRole(Request $request, Role $role)
+    {
+
+    }
+}
