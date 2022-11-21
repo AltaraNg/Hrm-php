051bab82c5abf5f90a42830a3199ca055c5f68b4
Omodamola Oladeji <darmhoo@hotmail.com>
2022-11-21 07:55:49 +0000

    migration edits

---
+++ database/migrations/2022_10_21_071152_update_roles_migration_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('roles_sales_categories', function (Blueprint $table) {
+            //
+            $table->dropForeign('roles_sales_categories_role_id_foreign');
+            // $table->unsignedBigInteger('role_id')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('roles_sales_categories', function (Blueprint $table) {
+            //
+        });
+    }
+};
